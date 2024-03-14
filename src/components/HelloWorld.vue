614ee736c9a951172c960f2eb2c4dfd45a8fc3fb
lsh <596060075@qq.com>
2024-03-14 17:13:42 +0800

    初始化项目,清理默认生成文件信息

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps<{ msg: string }>()
+</script>
+
+<template>
+  <div>
+    <h1>Hello world</h1>
+    <p>{{ props.msg}}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
